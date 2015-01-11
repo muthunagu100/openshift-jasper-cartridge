0159a6fd80bafe3ddd2678e24c18a35764c7905b
Muthu Nagu <muthunagu100@gmail.com>
2015-01-11 18:08:01 -0500

    Jasper Cartridge Creation

---
+++ apache-tomcat/bin/setenv.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/Applications/jasperreports-server-6.0.1/java
+JAVA_OPTS="$JAVA_OPTS "
+export JAVA_OPTS
+                            
---
+++ postgresql/bin/setenv.sh
@@ -0,0 +1,18 @@
+
+LD_LIBRARY_PATH="/Applications/jasperreports-server-6.0.1/postgresql/lib:/Applications/jasperreports-server-6.0.1/common/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH
+DYLD_LIBRARY_PATH="/Applications/jasperreports-server-6.0.1/postgresql/lib:/Applications/jasperreports-server-6.0.1/common/lib:$DYLD_LIBRARY_PATH"
+export DYLD_LIBRARY_PATH
+LC_MESSAGES=C
+export LC_MESSAGES
+        
+
+PGHOST=127.0.0.1
+export PGHOST
+PGPORT=5432
+export PGPORT
+PGUSER="postgres"
+export PGUSER
+PGDATA="/Applications/jasperreports-server-6.0.1/postgresql/data"
+export PGDATA
+        
